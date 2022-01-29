a94a56237fc114c79bb840484fb34334867c76cb
TylerIsMoore <91577312+TylerIsMoore@users.noreply.github.com>
2022-01-29 18:28:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let gondola = {quantity: 7, insured: true, operational: true, pricePer: 3800};
+let stockCar = {quantity: 1, insured: true, operational: false, pricePer: 6500};
+let boxCar = {quantity: 8, insured: true, operational: true, pricePer: 4157};
+let tankCar = {quantity: 3, insured: true, operational: true, pricePer: 7200};
+
+//Task 1: Get list of all cars reported as non operational (operational = false)
+var nonOperational = [];
+
+if (gondola.operational === false)
+{
+ nonOperational.push("gondola");
+}
+
+//Task 2: Get count of all cars with no Insurance
+
+//Task 3: Total dollar amount spent on tankCars, stockCars, boxCars over 6500.00
+// if the dollar amount spent on each ^ > 6500.00 add to total
+let totalAmountCars = 0
+if (tankCar.pricePer >= 6500)
+{
+  let tankCarTotal = tankCar.pricePer * tankCar.quantity;
+  totalAmountCars = totalAmountCars + tankCarTotal;
+}
+
+
+
